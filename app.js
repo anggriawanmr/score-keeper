5a419e2eed55dbdcdde76abe5c02cf03cc0a0492
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2021-04-26 13:51:36 +0700

    adding option value

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#resetButton');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const maxPlay = document.querySelector('#maxPlay');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -13,7 +15,9 @@ p1Button.addEventListener('click', function(e) {
     p1Score += 1;
 
     if(p1Score == winningScore) {
-      isGameOver = true;  
+      isGameOver = true;
+      p1Display.classList.add('winner');
+      p2Display.classList.add('loser');
     } 
 
     p1Display.innerText = p1Score;
@@ -26,8 +30,28 @@ p2Button.addEventListener('click', function(e) {
 
     if(p2Score == winningScore) {
       isGameOver = true;  
+      p1Display.classList.add('loser');
+      p2Display.classList.add('winner');
     } 
 
     p2Display.innerText = p2Score;
   }
 });
+
+maxPlay.addEventListener('change', function() {
+  winningScore = parseInt(this.value);
+  reset();
+
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+  isGameOver = false;
+  p2Score = 0;
+  p1Score = 0;
+  p1Display.innerText = 0;
+  p2Display.innerText = 0;
+  p1Display.classList.remove('loser', 'winner');
+  p2Display.classList.remove('winner', 'loser');
+}
